c651c1dc30e0782250c65de3e86dddfd214081c5
ness <nessfolio@gmail.com>
2026-06-29 13:23:42 +0600

    feat(server): implement registration

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,7 +1,9 @@
 const UserModel = require('../models/user-model')
 const bcrypt = require('bcrypt')
-const uuid = require('uuid')
+const { v4: uuidv4 } = require('uuid')
 const mailService = require('./mail-service')
+const tokenService = require('./token-service')
+const UserDTO = require('../dtos/user-dto')
 
 class UserService {
     async registration(email, password) {
@@ -9,11 +11,17 @@ class UserService {
         if (candidate) {
             throw new Error(`User with ${email} already exists`)
         }
+        const hashPassword = await bcrypt.hash(password, 10);
+        const activationLink = uuidv4()
 
-        const hashPassword = await bcrypt.hash(password, 3);
-        const activationLink = uuid.v4()
         const user = await UserModel.create({email, password: hashPassword, activationLink})
         await mailService.sendActivationMail(email, activationLink)
+        
+        const userDTO = new UserDTO(user); // id, email, isActivated
+        const tokens = tokenService.generateTokens({...userDTO});
+        await tokenService.saveToken(userDTO.id, tokens.refreshToken);
+
+        return {...tokens, user: userDTO}
     }
 }
 
