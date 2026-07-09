d9e74ed4aa59b2f37af1871dbe4fc821f74aa11a
ness <nessfolio@gmail.com>
2026-07-09 12:02:52 +0600

    feat: add validation with express-validator

--- server/router/index.js
+++ server/router/index.js
@@ -1,8 +1,12 @@
 const Router = require('express').Router;
 const userController = require('../controllers/user-controller');
 const router =  Router();
+const {body} = require('express-validator')
 
-router.post('/registration', userController.registration);
+router.post('/registration',
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({min: 8, max: 32}).withMessage('Password must be 8-32 characters'),
+    userController.registration);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
