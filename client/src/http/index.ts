ee74b45995d6507369c2d63afb68413ccd291dd8
ness <nessfolio@gmail.com>
2026-07-30 03:39:07 +0600

    feat: add response interceptor for access token refresh

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {AuthResponse} from "@/src/models/response/AuthResponse";
 
 export const API_URL = `http://localhost:9090/api`
 
@@ -10,6 +11,23 @@ const API = axios.create({
 API.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
     return config;
+})
+
+API.interceptors.response.use((config) => {
+    return config
+}, async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status == 401 && error.config && !error.config._isRetry) {
+        originalRequest._isRetry = true;
+        try {
+            const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, { withCredentials: true });
+            localStorage.setItem('token', response.data.accessToken)
+            return API.request(originalRequest);
+        } catch(e) {
+            console.log('Unauthorized', e)
+        }
+    }
+    throw error;
 })
 
 export default API;
